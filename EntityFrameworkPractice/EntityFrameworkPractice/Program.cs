7ff449a223167bc265c9d1fac5ba851e4d747cf0
hidetzu <hidetzu@gmail.com>
2016-09-22 13:11:37 +0900

    EntityFrameworkPractice 接続してデータを取得するサンプル

---
+++ EntityFrameworkPractice/EntityFrameworkPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using EntityFrameworkPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db = new NorthwindContext())
+            {
+                var products = from product in db.Products select product;
+                foreach(var p in products)
+                    Console.WriteLine("{0} | {1} | {2} | {3}",
+                 p.ProductID, p.ProductName, p.UnitPrice, p.QuantityPerUnit);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
